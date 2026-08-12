b9d93be6b8fd3ad99e6a9013dedb7f462725f0a5
Abhishekpatle <patlerajeshwari507@gmail.com>
2026-08-12 23:22:34 +0000

    Force upload ai_bot file

---
+++ ai_bot.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print(f"Welcome to college, {name}!")
+
+age = int(input("Enter your current age (e.g., 18): "))
+days_lived = age * 365
+
+print(f"Wow {name}, you have been alive for approximately {days_lived} days!")
